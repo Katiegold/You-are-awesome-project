0872d4fb7bc215775acf21c2a271a0bc56959737
K Gold <KatieGold@ks-macbook-pro.bc.edu>
2017-09-04 20:20:46 -0400

    the let statement

--- You are awesome/ViewController.swift
+++ You are awesome/ViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+      let pi = 3.1415926535897932384626
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var messageLabelOther: UILabel!
+    
+// This code executes when the view controller loads
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print(pi)
+        
+   
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+    
+        let message1 = "You are FANTASTIC!"
+        let message2 = "You are great!"
+        let message3 = "You are amazing!"
+      
+        
+        if messageLabel.text == message1 {
+            messageLabel.text = message2
+            print(pi)
+            
+        } else if messageLabel.text == message2{
+            messageLabel.text = message3
+    
+            
+        } else {
+            messageLabel.text = message1
+        }
+    
     }
 
-
+    
+ 
 }
 
